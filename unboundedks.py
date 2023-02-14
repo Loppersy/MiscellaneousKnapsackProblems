2f656ea75bbdf4f7a48e227e8afd58e7aa4a0df4
spacedmetal <jessen.harms.robinson@gmail.com>
2023-02-14 11:18:04 -0800

    added unbounded traceback, appears to be functioning correctly.  Still no reading/writing to file.  Test print for traceback in file

--- unboundedks.py
+++ unboundedks.py
@@ -46,6 +46,34 @@ def unboundedKS(weight_profit, max_capacity):
 
     return knapsack_arr
 
+def unboundedtraceback(weight_profit, knapsack_arr):
+    #unzip the (weight, profit tuples) for clarity
+    weight, profit = zip(*weight_profit)
+
+    table_w = len(knapsack_arr[0]) #table width (horizontal)
+    table_h = len(knapsack_arr) #table height (vertical)
+    
+    count = [0] * (table_h - 1) #holds the count for the number of times each item has been added
+
+    i = table_h - 1 #representative of the current item, starting from the end
+    j = table_w - 1 #representative of the currend capacity, starting with the greatest
+
+    #as long as we are not on the basecase of 0 items or capacity 0, continue matrix traceback
+    while(knapsack_arr[i][j]): 
+        #if the current cell is the same as the one above it, the current item has not been
+        #added to the knapsack for this total capacity, so change to the previous item
+        if(knapsack_arr[i][j] == knapsack_arr[i - 1][j]):
+            i -= 1 #decrement i to get the previous item
+        else:
+        #otherwise the current item has been added to the knapsack, increment it's count
+        #and go the the nearest horizontal cell less the current items' weight to see if
+        #it has been added again
+            count[i - 1] += 1 #increment the count for the current item
+            j = j - weight[i - 1] #move horizontally backwards by the weight of this item
+    
+    return count
+
+
 #temp hardcodes
 weight_profit = [(1, 2), (2, 5), (4, 8)]
 
@@ -53,3 +81,9 @@ capacity = 6
 
 result = unboundedKS(weight_profit, capacity)
 print(np.matrix(result))
+
+count = unboundedtraceback(weight_profit, result)
+for i in range(len(weight_profit)):
+    #if the count is >0 the corresponging i item has been selected
+    if(count[i] > 0):
+        print("Item ", weight_profit[i], " count ", count[i])
